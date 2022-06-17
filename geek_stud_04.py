a21e12d61e7baeb6b770f59ace5d0358ef72c3ff
Kotana73 <107360389+Kotana73@users.noreply.github.com>
2022-06-17 14:43:21 +0300

    Add files via upload

---
+++ geek_stud_04.py
@@ -0,0 +1,15 @@
+# вычисление большeй цифры числа
+n = int(input("Введите целое положительное число "))
+max1 = n % 10
+print(max1)
+while n >= 1:
+    n = n // 10
+    print(n)
+    if n % 10 > max1:
+        print(n)
+        print(max1)
+        max1 = n % 10
+        print(max1)
+    elif n > 9:
+        pass
+print("Максимальное цифра в числе ", max1)
